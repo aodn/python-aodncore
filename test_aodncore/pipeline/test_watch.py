aodn/python-aodncore

commit 8e0a07a2ca1e8e2ae658eb4cd706be43dddc614a

Change IncomingFileStateManager to a concrete class, since storage abstraction now occurs at the StorageBroker level, meaning no need for alternate implementations here. Change error_dir to error_uri and use a StorageBroker to manage/query files in error. Update tests.

aodncore/pipeline/watch.py
@@ -15,7 +15,6 @@ This is typically run as an operating system service by something like superviso
 for debugging.
 """
 
-import abc
 import logging.config
 import os
 import stat
@@ -25,7 +24,9 @@ from uuid import uuid4
 from six import PY2
 from transitions import Machine
 
+from .files import PipelineFile
 from .log import get_pipeline_logger
+from .storage import get_storage_broker
 from ..util import format_exception, mkdir_p, rm_f, rm_r, validate_dir_writable, validate_file_writable
 
 # OS X test compatibility, due to absence of pyinotify (which is specific to the Linux kernel)
@@ -133,32 +134,6 @@ class CeleryContext(object):
                 self.logger = None
                 self.pipeline_name = pipeline_name
 
-            def _generate_location_map(self, input_file):
-                basename = os.path.basename(input_file)
-                incoming_dir = os.path.dirname(input_file)
-                error_dir = os.path.join(config.pipeline_config['global']['error_dir'], pipeline_name)
-                error_path = os.path.join(error_dir,
-                                          "{name}.{id}".format(name=basename, id=self.request.id))
-                processing_dir = os.path.join(config.pipeline_config['global']['processing_dir'], pipeline_name,
-                                              self.request.id)
-                processing_path = os.path.join(processing_dir, basename)
-
-                return {
-                    'incoming': {
-                        'dir': incoming_dir,
-                        'path': input_file,
-                        'relative_path': os.path.relpath(input_file, config.pipeline_config['watch']['incoming_dir'])
-                    },
-                    'error': {
-                        'dir': error_dir,
-                        'path': error_path
-                    },
-                    'processing': {
-                        'dir': processing_dir,
-                        'path': processing_path
-                    }
-                }
-
             def _configure_logger(self):
                 logging.config.dictConfig(config.worker_logging_config)
                 logging_extra = {
@@ -171,15 +146,17 @@ class CeleryContext(object):
                 try:
                     self._configure_logger()
 
-                    location_map = self._generate_location_map(incoming_file)
-                    self.file_state_manager = IncomingFileStateManager(location_map, self.logger)
+                    self.file_state_manager = IncomingFileStateManager(input_file=incoming_file,
+                                                                       pipeline_name=pipeline_name,
+                                                                       config=config,
+                                                                       logger=self.logger,
+                                                                       celery_request=self.request)
 
                     self.file_state_manager.move_to_processing()
 
                     try:
-                        self.handler = handler_class(location_map['processing']['path'], celery_task=self,
-                                                     config=config,
-                                                     upload_path=location_map['incoming']['relative_path'],
+                        self.handler = handler_class(self.file_state_manager.processing_path, celery_task=self,
+                                                     config=config, upload_path=self.file_state_manager.relative_path,
                                                      **kwargs)
                     except Exception as e:
                         self.file_state_manager.move_to_error()
@@ -308,8 +285,9 @@ class IncomingFileEventHandler(pyinotify.ProcessEvent):
         self._logger.debug("full task_data: {task_data}".format(task_data=task_data))
 
 
-class AbstractFileStateManager(object):
-    __metaclass__ = abc.ABCMeta
+class IncomingFileStateManager(object):
+    processing_mode = stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
+    error_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH
 
     states = ['FILE_IN_INCOMING', 'FILE_IN_PROCESSING', 'FILE_IN_ERROR', 'FILE_SUCCESS']
     transitions = [
@@ -333,62 +311,91 @@ class AbstractFileStateManager(object):
         }
     ]
 
-    def __init__(self, location_map, logger):
-        self.location_map = location_map
+    def __init__(self, input_file, pipeline_name, config, logger, celery_request, error_broker=None):
+        self.input_file = input_file
+        self.pipeline_name = pipeline_name
+        self.config = config
         self.logger = logger
+        self.celery_request = celery_request
+
         self._machine = Machine(model=self, states=self.states, initial='FILE_IN_INCOMING', auto_transitions=False,
                                 transitions=self.transitions, after_state_change='_after_state_change')
         self.logger.sysinfo(
             "{name} initialised in state: {state}".format(name=self.__class__.__name__, state=self.state))
 
+        self._error_broker = error_broker
+
         self._pre_check()
+
+    @property
+    def error_broker(self):
+        if self._error_broker is None:
+            self._error_broker = get_storage_broker(self.error_uri)
+        return self._error_broker
+
+    @property
+    def basename(self):
+        return os.path.basename(self.input_file)
+
+    @property
+    def incoming_dir(self):
+        return os.path.dirname(self.input_file)
+
+    @property
+    def processing_dir(self):
+        return os.path.join(self.config.pipeline_config['global']['processing_dir'], self.pipeline_name,
+                            self.celery_request.id)
+
+    @property
+    def processing_path(self):
+        return os.path.join(self.processing_dir, self.basename)
+
+    @property
+    def relative_path(self):
+        return os.path.relpath(self.input_file, self.config.pipeline_config['watch']['incoming_dir'])
+
+    @property
+    def error_name(self):
+        return "{name}.{id}".format(name=self.basename, id=self.celery_request.id)
+
+    @property
+    def error_uri(self):
+        return os.path.join(self.config.pipeline_config['global']['error_uri'], self.pipeline_name)
 
     def _after_state_change(self):
         self.logger.sysinfo(
             "{name} transitioned to state: {state}".format(name=self.__class__.__name__, state=self.state))
 
-    @abc.abstractmethod
     def _pre_check(self):
-        pass
+        try:
+            validate_file_writable(self.input_file)
 
-    @abc.abstractmethod
-    def _move_to_processing(self):
-        pass
+            mkdir_p(self.processing_dir)
+            validate_dir_writable(self.processing_dir)
 
-    @abc.abstractmethod
-    def _move_to_error(self):
-        pass
-
-    @abc.abstractmethod
-    def _cleanup_success(self):
-        pass
-
-
-class IncomingFileStateManager(AbstractFileStateManager):
-    processing_mode = stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
-    error_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH
-
-    def _pre_check(self):
-        validate_file_writable(self.location_map['incoming']['path'])
-
-        mkdir_p(self.location_map['processing']['dir'])
-        validate_dir_writable(self.location_map['processing']['dir'])
-
-        mkdir_p(self.location_map['error']['dir'])
-        validate_dir_writable(self.location_map['error']['dir'])
+            # TODO: better validation of broker usability?
+            _ = self.error_broker.query('')
+        except Exception:  # pragma: no cover
+            self.logger.exception('exception occurred initialising IncomingFileStateManager')
+            raise
 
     def _move_to_processing(self):
-        safe_move_file(self.location_map['incoming']['path'], self.location_map['processing']['path'])
-        os.chmod(self.location_map['processing']['path'], self.processing_mode)
+        self.logger.info("{name}.move_to_processing -> '{path}'".format(name=self.__class__.__name__,
+                                                                        path=self.processing_path))
+        safe_move_file(self.input_file, self.processing_path)
+        os.chmod(self.processing_path, self.processing_mode)
 
     def _move_to_error(self):
-        safe_move_file(self.location_map['processing']['path'], self.location_map['error']['path'])
-        os.chmod(self.location_map['error']['path'], self.error_mode)
-        rm_r(self.location_map['processing']['dir'])
+        full_error_path = os.path.join(self.error_broker.prefix, self.error_name)
+        self.logger.info("{name}.move_to_error -> '{path}'".format(name=self.__class__.__name__,
+                                                                   path=full_error_path))
+        error_file = PipelineFile(self.processing_path, dest_path=self.error_name)
+        self.error_broker.upload(error_file)
+        rm_f(self.processing_path)
 
     def _cleanup_success(self):
-        rm_f(self.location_map['processing']['path'])
-        rm_r(self.location_map['processing']['dir'])
+        rm_f(self.processing_path)
+        rm_r(self.processing_dir)
 
 
 class WatchServiceContext(object):

test_aodncore/pipeline/test_watch.py
@@ -1,5 +1,9 @@
-from aodncore.pipeline.watch import (get_task_name, CeleryConfig)
+import os
+
+from aodncore.pipeline.log import get_pipeline_logger
+from aodncore.pipeline.watch import get_task_name, CeleryConfig, IncomingFileStateManager
 from aodncore.testlib import BaseTestCase
+from aodncore.util import safe_copy_file
 
 
 class TestPipelineWatch(BaseTestCase):
@@ -25,9 +29,62 @@ class TestCeleryManager(BaseTestCase):
         self.celeryconfig = CeleryConfig()
 
 
+class TestIncomingFileStateManager(BaseTestCase):
+    def setUp(self):
+        self.logger = get_pipeline_logger('unittest')
+        incoming_file_path = os.path.join(self.config.pipeline_config['watch']['incoming_dir'],
+                                          os.path.basename(self.temp_nc_file))
+        safe_copy_file(self.temp_nc_file, incoming_file_path)
+        celery_request = type('DummyRequest', (object,), {'id': 'NO_REQUEST_ID'})()
+        self.state_manager = IncomingFileStateManager(incoming_file_path, pipeline_name='UNITTEST', config=self.config,
+                                                      logger=self.logger, celery_request=celery_request)
+
+    def test_error(self):
+        self.assertTrue(os.path.exists(self.state_manager.input_file))
+        self.assertFalse(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertNotIn(self.state_manager.error_name, error_result)
+
+        self.state_manager.move_to_processing()
+
+        self.assertFalse(os.path.exists(self.state_manager.input_file))
+        self.assertTrue(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertNotIn(self.state_manager.error_name, error_result)
+
+        self.state_manager.move_to_error()
+
+        self.assertFalse(os.path.exists(self.state_manager.input_file))
+        self.assertFalse(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertIn(self.state_manager.error_name, error_result)
+
+    def test_success(self):
+        self.assertTrue(os.path.exists(self.state_manager.input_file))
+        self.assertFalse(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertNotIn(self.state_manager.error_name, error_result)
+
+        self.state_manager.move_to_processing()
+
+        self.assertFalse(os.path.exists(self.state_manager.input_file))
+        self.assertTrue(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertNotIn(self.state_manager.error_name, error_result)
+
+        self.state_manager.move_to_success()
+
+        self.assertFalse(os.path.exists(self.state_manager.input_file))
+        self.assertFalse(os.path.exists(self.state_manager.processing_path))
+        error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
+        self.assertNotIn(self.state_manager.error_name, error_result)
+
+
 class TestIncomingFileEventHandler(BaseTestCase):
+    # TODO: write tests
     pass
 
 
 class TestInotifyManager(BaseTestCase):
+    # TODO: write tests
     pass

aodncore/pipeline/schema.py
@@ -84,14 +84,14 @@ PIPELINE_CONFIG_SCHEMA = {
                     'items': {'type': 'string'}
                 },
                 'archive_uri': {'type': 'string'},
-                'error_dir': {'type': 'string'},
+                'error_uri': {'type': 'string'},
                 'opendap_root': {'type': 'string'},
                 'processing_dir': {'type': 'string'},
                 'tmp_dir': {'type': 'string'},
                 'upload_uri': {'type': 'string'},
                 'wip_dir': {'type': 'string'}
             },
-            'required': ['admin_recipients', 'archive_uri', 'error_dir', 'processing_dir', 'upload_uri', 'wip_dir'],
+            'required': ['admin_recipients', 'archive_uri', 'error_uri', 'processing_dir', 'upload_uri', 'wip_dir'],
             'additionalProperties': False
         },
         'logging': {

aodncore/testlib/conf/pipeline.conf
@@ -2,7 +2,7 @@
   "global": {
     "admin_recipients": ["unittest:dummy"],
     "archive_uri": "file:///tmp/probably/doesnt/exist/archive",
-    "error_dir": "/tmp/probably/doesnt/exist/error",
+    "error_uri": "file:///tmp/probably/doesnt/exist/error",
     "opendap_root": "http://opendap.example.com",
     "processing_dir": "/tmp/probably/doesnt/exist/processing",
     "upload_uri": "file:///tmp/probably/doesnt/exist/upload",

aodncore/testlib/testutil.py
@@ -201,13 +201,14 @@ def load_runtime_patched_pipeline_config_file(config_file, rel_path, temp_dir):
 
     temp_upload_dir = tempfile.mkdtemp(prefix='temp_upload_uri', dir=temp_dir)
 
-    pipeline_config['global']['error_dir'] = tempfile.mkdtemp(prefix='temp_error_dir', dir=temp_dir)
+    pipeline_config['global']['error_uri'] = "file://{}".format(tempfile.mkdtemp(prefix='temp_error_dir', dir=temp_dir))
     pipeline_config['global']['processing_dir'] = tempfile.mkdtemp(prefix='temp_processing_dir', dir=temp_dir)
     pipeline_config['global']['tmp_dir'] = temp_dir
     pipeline_config['global']['upload_uri'] = urlunsplit(('file', None, temp_upload_dir, None, None))
     pipeline_config['global']['wip_dir'] = rel_path
     pipeline_config['logging']['log_root'] = tempfile.mkdtemp(prefix='temp_log_root', dir=temp_dir)
     pipeline_config['mail']['smtp_server'] = str(uuid.uuid4())
+    pipeline_config['watch']['incoming_dir'] = tempfile.mkdtemp(prefix='temp_incoming_dir', dir=temp_dir)
 
     # reload the JSON object with non-updatable keys
     return json.loads(json.dumps(pipeline_config), object_pairs_hook=WriteOnceOrderedDict)

test_aodncore/pipeline/test_configlib.py
@@ -15,7 +15,7 @@ REFERENCE_PIPELINE_CONFIG = {
     'global': {
         'admin_recipients': ['unittest:dummy'],
         'archive_uri': 'file:///tmp/probably/doesnt/exist/archive',
-        'error_dir': '/tmp/probably/doesnt/exist/error',
+        'error_uri': 'file:///tmp/probably/doesnt/exist/error',
         'opendap_root': 'http://opendap.example.com',
         'processing_dir': '/tmp/probably/doesnt/exist/processing',
         'upload_uri': 'file:///tmp/probably/doesnt/exist/upload',
@@ -123,10 +123,6 @@ REFERENCE_WATCH_CONFIG = {
     }
 }
 
-REFERENCE_WATCH_DIRECTORY_MAP = {
-    '/tmp/probably/doesnt/exist/incoming/ANMN/QLD/XXXX': 'ANMN_QLD_XXXX'
-}
-
 
 class TestLazyConfigManager(BaseTestCase):
     def setUp(self):
@@ -143,7 +139,8 @@ class TestLazyConfigManager(BaseTestCase):
 
     def test_watch_directory_map(self):
         self.assertIsNone(self.config._watch_directory_map)
-        self.assertDictEqual(REFERENCE_WATCH_DIRECTORY_MAP, self.config.watch_directory_map)
+        expected_map = {os.path.join(self.config.pipeline_config['watch']['incoming_dir'], 'ANMN/QLD/XXXX'): 'ANMN_QLD_XXXX'}
+        self.assertDictEqual(expected_map, self.config.watch_directory_map)
 
     def test_purge_lazy_properties(self):
         _ = self.config.celery_application

commit 56ddd83edf59254bb99ac95dfec8cc9bc7d17b51

Fixes https://github.com/aodn/python-aodncore/issues/124. Reinstate setting of error_mode after moving file to error. Bug introduced in https://github.com/aodn/python-aodncore/commit/8e0a07a2ca1e8e2ae658eb4cd706be43dddc614a#diff-d33a9b2c17ca2d3f87788ecfd8c72676

aodncore/pipeline/storage.py
@@ -57,6 +57,7 @@ class BaseStorageBroker(object):
 
     def __init__(self):
         self.prefix = None
+        self.mode = None
 
     def __repr__(self):
         return "{name}(prefix='{prefix}')".format(name=self.__class__.__name__, prefix=self.prefix)
@@ -235,6 +236,8 @@ class LocalFileStorageBroker(BaseStorageBroker):
         abs_path = self._get_absolute_dest_path(pipeline_file=pipeline_file, dest_path_attr=dest_path_attr)
         mkdir_p(os.path.dirname(abs_path))
         safe_copy_file(pipeline_file.src_path, abs_path, overwrite=True)
+        if self.mode:
+            os.chmod(abs_path, self.mode)
 
 
 class S3StorageBroker(BaseStorageBroker):

aodncore/pipeline/watch.py
@@ -423,6 +423,7 @@ class IncomingFileStateManager(object):
     def error_broker(self):
         if self._error_broker is None:
             self._error_broker = get_storage_broker(self.error_uri)
+            self._error_broker.mode = self.error_mode
             self.logger.info("{name}.error_broker -> {broker}".format(name=self.__class__.__name__,
                                                                       broker=self._error_broker))
         return self._error_broker

test_aodncore/pipeline/test_watch.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import stat
 
 from aodncore.pipeline import PipelineFile, PipelineFileCollection
 from aodncore.pipeline.log import get_pipeline_logger
@@ -134,13 +135,18 @@ class TestIncomingFileStateManager(BaseTestCase):
         self.assertTrue(os.path.exists(self.state_manager.processing_path))
         error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
         self.assertNotIn(self.state_manager.error_name, error_result)
+        self.assertEqual(stat.S_IMODE(os.stat(self.state_manager.processing_path).st_mode),
+                         self.state_manager.processing_mode)
 
         self.state_manager.move_to_error()
+
+        full_error_path = os.path.join(self.state_manager.error_broker.prefix, self.state_manager.error_name)
 
         self.assertFalse(os.path.exists(self.state_manager.input_file))
         self.assertFalse(os.path.exists(self.state_manager.processing_path))
         error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
         self.assertIn(self.state_manager.error_name, error_result)
+        self.assertEqual(stat.S_IMODE(os.stat(full_error_path).st_mode), self.state_manager.error_mode)
 
     def test_success(self):
         self.assertTrue(os.path.exists(self.state_manager.input_file))
@@ -154,6 +160,8 @@ class TestIncomingFileStateManager(BaseTestCase):
         self.assertTrue(os.path.exists(self.state_manager.processing_path))
         error_result = self.state_manager.error_broker.query(self.state_manager.error_name)
         self.assertNotIn(self.state_manager.error_name, error_result)
+        self.assertEqual(stat.S_IMODE(os.stat(self.state_manager.processing_path).st_mode),
+                         self.state_manager.processing_mode)
 
         self.state_manager.move_to_success()
 