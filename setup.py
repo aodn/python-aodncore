b7f70a78850f95f1917c096e7d74eeded30e8bc6
aodn-ci <info@aodn.org.au>
2019-12-19 22:56:51 +0000

    Bump version to 1.0.10

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ PACKAGE_SCRIPTS = ['aodncore/bin/drawmachine.py', 'aodncore/pipeline/watchservic
 
 setup(
     name=PACKAGE_NAME,
-    version='1.0.9',
+    version='1.0.10',
     scripts=PACKAGE_SCRIPTS,
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     package_data=PACKAGE_DATA,
