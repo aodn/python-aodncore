4b89e72eb28dd36a985cf251db1a842295355726
Leigh Gordon <leigh.gordon@utas.edu.au>
2020-05-19 12:11:21 +1000

    Reorder StateQuery methods and fix docstrings

--- aodncore/pipeline/statequery.py
+++ aodncore/pipeline/statequery.py
@@ -15,6 +15,7 @@ class StateQuery(object):
         self._storage_broker = storage_broker
         self._wfs_broker = wfs_broker
 
+    # WFS methods
     @property
     def wfs(self):
         """Read-only property to access the instantiated WebFeatureService object
@@ -22,14 +23,6 @@ class StateQuery(object):
         :return: WebFeatureService instance
         """
         return self._wfs_broker.wfs
-
-    def query_storage(self, query):  # pragma: no cover
-        """Query the storage backend and return existing files matching the given query
-
-        :param query: S3-style prefix for filtering query results
-        :return: dict containing the query results
-        """
-        return self._storage_broker.query(query)
 
     def query_wfs_getfeature_dict(self, **kwargs):
         """Make a GetFeature request, and return the response in a native dict
@@ -63,12 +56,20 @@ class StateQuery(object):
         """
         return self._wfs_broker.query_url_exists(layer, name)
 
+    # Storage methods
     def download(self, remotepipelinefilecollection, local_path):
-        """Helper method to download a RemotePipelineFileCollection or RemotePipelineFile using the handler's internal
-            storage broker
+        """Helper method to download a RemotePipelineFileCollection or RemotePipelineFile
 
         :param remotepipelinefilecollection: RemotePipelineFileCollection to download
         :param local_path: local path where files will be downloaded. Defaults to the handler's :attr:`temp_dir` value.
         :return: None
         """
         self._storage_broker.download(remotepipelinefilecollection, local_path)
+
+    def query_storage(self, query):  # pragma: no cover
+        """Query the storage backend and return existing files matching the given query
+
+        :param query: S3-style prefix for filtering query results
+        :return: dict containing the query results
+        """
+        return self._storage_broker.query(query)
