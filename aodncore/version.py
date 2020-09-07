4e1e282d9175b1ab30cd4d56480ada693cd8f72f
aodn-ci <info@aodn.org.au>
2020-09-07 07:16:08 +0000

    Bump version to 1.1.5

--- aodncore/version.py
+++ aodncore/version.py
@@ -2,4 +2,4 @@
 Updated automatically by the build server
 """
 
-__version__ = '1.1.4'
+__version__ = '1.1.5'
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ PACKAGE_SCRIPTS = ['aodncore/bin/drawmachine.py', 'aodncore/pipeline/watchservic
 
 setup(
     name=PACKAGE_NAME,
-    version='1.1.4',
+    version='1.1.5',
     scripts=PACKAGE_SCRIPTS,
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     package_data=PACKAGE_DATA,
