ddf17b6712561a8f2e6e5799a655ad36276bf2b4
lbesnard <laurent.besnard@utas.edu.au>
2020-12-10 15:11:09 +1100

    fix: mock existing vocab unittest

--- test_aodncore/vocab/test_platform_code_vocab.py
+++ test_aodncore/vocab/test_platform_code_vocab.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import patch
 
 from aodncore.testlib import BaseTestCase
 from aodncore.vocab.platform_code_vocab import (PlatformVocabHelper, platform_type_uris_by_category,
@@ -9,8 +10,8 @@ TEST_ROOT = os.path.join(os.path.dirname(__file__))
 PLATFORM_CAT_VOCAB_URL = '%s%s' % ('file://',
                                    os.path.join(TEST_ROOT, 'aodn_aodn-platform-category-vocabulary.rdf'))
 
-DEFAULT_PLATFORM_VOCAB_URL = '%s%s' % ('file://',
-                                       os.path.join(TEST_ROOT, 'aodn_aodn-platform-vocabulary.rdf'))
+PLATFORM_VOCAB_URL = '%s%s' % ('file://',
+                               os.path.join(TEST_ROOT, 'aodn_aodn-platform-vocabulary.rdf'))
 
 
 class TestPlatformCodeVocab(BaseTestCase):
@@ -29,6 +30,7 @@ class TestPlatformCodeVocab(BaseTestCase):
         res = platform_type_uris_by_category(platform_cat_vocab_url=PLATFORM_CAT_VOCAB_URL)
         self.assertEqual(res['Float'][0], 'http://vocab.nerc.ac.uk/collection/L06/current/42')
 
+    @patch("aodncore.vocab.platform_code_vocab.DEFAULT_PLATFORM_CAT_VOCAB_URL", PLATFORM_CAT_VOCAB_URL)
     def test_altlabels_deprecated(self):
-        res = platform_altlabels_per_preflabel(category_name='Vessel', platform_vocab_url=DEFAULT_PLATFORM_VOCAB_URL)
+        res = platform_altlabels_per_preflabel(category_name='Vessel', platform_vocab_url=PLATFORM_VOCAB_URL)
         self.assertEqual(res['9VUU'], 'Anro Asia')
