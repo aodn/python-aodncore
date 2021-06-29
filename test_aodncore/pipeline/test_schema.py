899c0cf53b3a9dd6c3e34995ddd7798e82ff693d
ndhogan <nate.hogan@utas.edu.au>
2021-06-29 17:46:42 +1000

    initial unittests for csv harvest code changes

--- test_aodncore/pipeline/test_schema.py
+++ test_aodncore/pipeline/test_schema.py
@@ -3,13 +3,14 @@ import os
 
 from jsonschema.exceptions import ValidationError
 
-from aodncore.pipeline.schema import validate_json_manifest
+from aodncore.pipeline.schema import validate_json_manifest, validate_harvest_params
 from aodncore.testlib import BaseTestCase
 from test_aodncore import TESTDATA_DIR
 
 GOOD_MANIFEST = os.path.join(TESTDATA_DIR, 'test.json_manifest')
 BAD_MANIFEST = os.path.join(TESTDATA_DIR, 'invalid.json_manifest')
-
+GOOD_HARVEST = os.path.join(TESTDATA_DIR, 'test.harvest_params')
+BAD_HARVEST = os.path.join(TESTDATA_DIR, 'invalid.harvest_params')
 
 class TestPipelineSchema(BaseTestCase):
     def test_validate_json_manifest_valid(self):
@@ -25,3 +26,17 @@ class TestPipelineSchema(BaseTestCase):
 
         with self.assertRaises(ValidationError):
             validate_json_manifest(content)
+
+    def test_validate_harvest_params_valid(self):
+        with open(GOOD_HARVEST) as f:
+            content = json.load(f)
+
+        with self.assertNoException():
+            validate_harvest_params(content)
+
+    def test_validate_harvest_params_invalid(self):
+        with open(BAD_HARVEST) as f:
+            content = json.load(f)
+
+        with self.assertRaises(ValidationError):
+            validate_harvest_params(content)
