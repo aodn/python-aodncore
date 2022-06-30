299069d7405e3c42cb2c793475d2e1975847c61e
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-07-01 00:34:57 +1000

    Remove SMTP client init exception

--- aodncore/pipeline/steps/notify.py
+++ aodncore/pipeline/steps/notify.py
@@ -277,12 +277,9 @@ class EmailNotifyRunner(BaseNotifyRunner):
         return message
 
     def _send(self, recipient_addresses, message):
-        try:
-            smtp_server = smtplib.SMTP(host=self._config.pipeline_config['mail']['smtp_server'],
-                                       port=self._config.pipeline_config['mail'].get('smtp_port', 587),
-                                       timeout=60)
-        except Exception as e:
-            self._logger.warning("Exception thrown when instantiating connection. {e}".format(e=format_exception(e)))
+        smtp_server = smtplib.SMTP(host=self._config.pipeline_config['mail']['smtp_server'],
+                                   port=self._config.pipeline_config['mail'].get('smtp_port', 587),
+                                   timeout=60)
         sendmail_result = None
         try:
             smtp_server.connect(self._config.pipeline_config['mail']['smtp_server'],
