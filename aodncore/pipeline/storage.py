9ac8b5c5c49269ff73f384f92811a67b031b0b91
cmrose <cmrose@utas.edu.au>
2023-07-13 16:19:49 +1000

    strip leading / from s3 prefixes

--- aodncore/pipeline/storage.py
+++ aodncore/pipeline/storage.py
@@ -324,7 +324,7 @@ class S3StorageBroker(BaseStorageBroker):
         super().__init__()
 
         self.bucket = bucket
-        self.prefix = prefix
+        self.prefix = prefix.strip('/')
 
         self.s3_client = boto3.client('s3')
 
